0776b499981f8443106cc6adaeb712ada4e45ef1
Arturo Filio Villa <arturfil@hotmail.com>
2017-10-28 18:28:14 -0400

    created the app-routing module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//services
+
+//components
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
